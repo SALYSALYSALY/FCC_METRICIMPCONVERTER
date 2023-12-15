ab9e24ced39929ffecb8d46276de84470184c48b
Saly <swalynerii@gmail.com>
2023-12-15 00:30:42 -0600

    Finish project

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -5,5 +5,10 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 let convertHandler = new ConvertHandler();
 
 suite('Unit Tests', function(){
-
+      suite('Function convertHandler.getNum(input)', () => {
+    test('Whole number input', done => {
+      const input = '32L';
+      assert.equal(convertHandler.getNum(input), 32);
+      done();
+    });
 });
